43a80dd608b5b89213c35dae50ccc0cd41210d7b
Ldthovn <ledinhtho98bn@gmail.com>
2020-04-26 14:35:05 +0700

    a

--- react-hooks-basic/src/components/Clock/index.jsx
+++ react-hooks-basic/src/components/Clock/index.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import useCLock from "../../hooks/useClock";
 
-// function formatDate(date) {
-//   const hours = `0${date.getHours()}`.slice(-2);
-//   const minutes = `0${date.getMinutes()}`.slice(-2);
-//   const seconds = `0${date.getSeconds()}`.slice(-2);
-//   return `${hours}:${minutes}:${seconds}`;
-// }
-
 function CLock(props) {
   const { timeString } = useCLock();
 
